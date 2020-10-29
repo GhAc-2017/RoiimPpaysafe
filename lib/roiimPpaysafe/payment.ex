5e1f64841ab45b20a906ad8ae809740b7bd8d977
Ayan <ayan.chowdhury2050@gmail.com>
2020-10-30 04:37:04 +0530

    added auto payment processing

--- lib/roiimPpaysafe/payment.ex
+++ lib/roiimPpaysafe/payment.ex
@@ -10,39 +10,99 @@ defmodule RoiimPpaysafe.Payment do
           }"
         )
       }",
-      "Content-Type": "application/json",
-      Simulator: "EXTERNAL"
+    "Content-Type": "application/json",
+    Simulator: "EXTERNAL"
   ]
+
+  def merchant_ref_generator(length\\32) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
+  end
 
   def process_payment() do
     url = "#{@paysafe_base_url}paymenthub/v1/payments"
-    details = Jason.encode!(%{
-      merchantRefNum: "aryan113",
-      merchantCustomerId: "roiim1000",
-      amount: 30000,
-      currencyCode: "USD",
-      paymentHandleToken: "SCQw7OwVEptkkD8M",
-      description: "paying you"
-    })
+    token = generate_payement_handle_token()
+
+    details =
+      Jason.encode!(%{
+        merchantRefNum: merchant_ref_generator(),
+        merchantCustomerId: "roiim1000",
+        amount: 3000,
+        currencyCode: "USD",
+        paymentHandleToken: token,
+        description: "paying you"
+      })
 
     case HTTPoison.post(url, details, @paysafe_header) do
       {:ok, %HTTPoison.Response{body: response_body}} ->
         case Jason.decode(response_body) do
           {:ok, result} ->
             case Map.has_key?(result, "error") do
-              true -> {:error, result}
-              _-> {:ok, result}
+              true ->
+                {:error, result}
+              _ ->
+                {:ok, "transaction #{result["id"]} completed successfully"}
             end
 
-          _->
+          _ ->
             {:error, "not_found"}
         end
 
-      _->
+      _ ->
         {:error, "not_found"}
     end
   end
 
   def generate_payement_handle_token() do
+    url = "#{@paysafe_base_url}paymenthub/v1/paymenthandles"
+
+    payment_req = %{
+      merchantRefNum: merchant_ref_generator(),
+      transactionType: "PAYMENT",
+      card: %{
+        cardNum: "5191330000004415",
+        cardExpiry: %{
+          month: 10,
+          year: 2020
+        },
+        cvv: "111",
+        holderName: "Ayan"
+      },
+      paymentType: "CARD",
+      amount: 3000,
+      currencyCode: "USD",
+      customerIp: "172.0.0.1",
+      billingDetails: %{
+        street: "100 Queen",
+        city: "Toronto",
+        zip: "M5H 2N2",
+        country: "CA"
+      },
+      returnLinks: [
+        %{
+          rel: "on_completed",
+          href: "https://amazon.com/payment/return/success",
+          method: "GET"
+        },
+        %{
+          rel: "on_failed",
+          href: "https://amazon.com/payment/return/failed",
+          method: "GET"
+        },
+        %{
+          rel: "default",
+          href: "https://amazon.com/payment/return/failed",
+          method: "GET"
+        }
+      ]
+    }
+
+    case HTTPoison.post(url, Jason.encode!(payment_req), @paysafe_header) do
+      {:ok, %HTTPoison.Response{body: response_body}} ->
+        result = Jason.decode!(response_body)
+        result["paymentHandleToken"]
+
+      _->
+        generate_payement_handle_token()
+    end
   end
 end
--- lib/roiimPpaysafe_web/controllers/payment_controller.ex
+++ lib/roiimPpaysafe_web/controllers/payment_controller.ex
@@ -1,6 +1,27 @@
 defmodule RoiimPpaysafeWeb.PaymentController do
   use RoiimPpaysafeWeb, :controller
+  alias RoiimPpaysafe.Payment
   def new(conn, _params) do
     render conn, "new.html"
+  end
+
+  def create(conn, _params) do
+    # require IEx
+    # IEx.pry
+    case Payment.process_payment() do
+      {:ok, res} ->
+        require IEx
+        IEx.pry
+        IO.puts(res)
+        conn
+        |> put_flash(:info, "#{res} Payment done!")
+      {:error, _coz} ->
+        render conn, "new.html"
+    end
+
+  end
+
+  def index() do
+
   end
 end
